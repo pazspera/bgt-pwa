da46bdca7f5a5a4cc06c9399942c0910d3d39caf
Paz Spera <mspera03@gmail.com>
2025-10-12 16:51:25 +0000

    style: Quitado border radius de los botones de BottomNavigation

--- src/components/organisms/BottomNavigation.vue
+++ src/components/organisms/BottomNavigation.vue
@@ -1,21 +1,21 @@
 <template>
   <v-bottom-navigation class="bg-neutral-800 bottom-nav" color="primary-300">
     <router-link class="nav-link" :to="{ name: 'BoardGames' }">
-      <v-btn value="ludoteca">
+      <v-btn value="ludoteca" class="bottom-nav__button">
         <i class="fa-solid fa-dice bottom-nav__icon"></i>
         <span class="mb-2">Ludoteca</span>
       </v-btn>
     </router-link>
 
     <router-link class="nav-link" :to="{ name: 'Players' }">
-      <v-btn value="jugadores">
+      <v-btn value="jugadores" class="bottom-nav__button">
         <i class="fa-solid fa-users bottom-nav__icon"></i>
         <span class="mb-2">Jugadores</span>
       </v-btn>
     </router-link>
 
     <router-link class="nav-link" :to="{ name: 'Games' }">
-      <v-btn value="partidas">
+      <v-btn value="partidas" class="bottom-nav__button">
         <i class="fa-solid fa-box-archive bottom-nav__icon"></i>
         <span class="mb-2">Partidas</span>
       </v-btn>
@@ -31,7 +31,7 @@ defineOptions({ name: "BottomNavigation" });
 
 .bottom-nav {
   height: 100%;
-  width: 100%;
+  max-width: 100%;
   height: var(--bottom-nav-height) !important;
   padding: 0 !important;
   z-index: 999;
@@ -44,5 +44,12 @@ defineOptions({ name: "BottomNavigation" });
 
 .nav-link {
   margin-bottom: var(--size9);
+}
+
+.bottom-nav__button {
+  border-bottom-left-radius: 0px;
+  border-bottom-right-radius: 0px;
+  border-top-left-radius: 0px;
+  border-top-right-radius: 0px;
 }
 </style>
