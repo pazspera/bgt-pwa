37dc50d84e33cf0264622524da615fd24511c7c8
Paz Spera <mspera03@gmail.com>
2025-10-13 15:42:45 +0000

    feat: Agregada configuración para poder probar app desde celular

--- vite.config.js
+++ vite.config.js
@@ -29,5 +29,10 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    // permite probar en mobile
   }
 })
