8d5748df0203e19e5c0008d2bfcadc530b8d9386
Paz Spera <mspera03@gmail.com>
2025-10-14 19:37:16 +0000

    refactor: Agregado ThemeToggler a BottomNavigation

--- src/components/organisms/BottomNavigation.vue
+++ src/components/organisms/BottomNavigation.vue
@@ -2,6 +2,7 @@
 import { faDice, faUsers, faBoxArchive } from "@fortawesome/free-solid-svg-icons";
 import { NavButton } from "../../types/navigation";
 import BottomNavButton from "../atoms/buttons/BottomNavButton.vue";
+import ThemeToggler from "../molecules/ThemeToggler.vue";
 
 defineOptions({ name: "BottomNavigation" });
 
@@ -31,6 +32,7 @@ const BottomNav: NavButton[] = [
 <template>
   <v-bottom-navigation tag="div" class="bottom-nav" grow>
     <BottomNavButton v-for="button in BottomNav" v-bind="button" :key="button.value" />
+    <ThemeToggler/>
   </v-bottom-navigation>
 </template>
 
