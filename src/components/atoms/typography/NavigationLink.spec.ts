baa1e61667163cca4e54300f492634e0731e8008
Paz Spera <mspera03@gmail.com>
2025-10-28 05:27:55 +0000

    tests: Agregados tests para verificar texto, clase base y route :to.

--- src/components/atoms/typography/NavigationLink.spec.ts
+++ src/components/atoms/typography/NavigationLink.spec.ts
@@ -3,9 +3,11 @@ import { mount } from "@vue/test-utils";
 import NavigationLink from "./NavigationLink.vue";
 import { router } from "../../../tests/utils/createRouterMock";
 
+const classNavLink = "navigation-link";
+
 const mountNavigationLink = (
   props = {}, 
-  slots={ default: "Test" }
+  slots= { default: "Test" }
 ) => {
   return mount(NavigationLink, {
     props: {
@@ -33,11 +35,38 @@ const mountNavigationLink = (
   })
 }
 
-it.todo("displays text", ()=> {});
+it("displays text", ()=> {
+  // Arrange
+  const text = "Link test";
+  const wrapper = mountNavigationLink({}, { default: text });
+  
+  // Assert
+  expect(wrapper.html()).toContain(text);
+});
 
-it.todo("has base css class", ()=> {});
+it("has base css class", ()=> {
+  // Arrange
+  const wrapper = mountNavigationLink();
 
-it.todo("receives valid :to route as an object", ()=> {});
+  // Assert
+  expect(wrapper.html()).toContain(classNavLink);
+});
+
+it("receives valid :to route as an object", ()=> {
+  // Arrange
+  const testRoute = { name: "EditPlayer", params: { id: 36 }};
+  const wrapper = mountNavigationLink({ to: testRoute });
+  // the stub is needed, the wrapper doesn't have the "to"
+  const navigationLinkStub = wrapper.find('[data-test="router-link-stub"]')
+
+  // Assert
+  // - checks that the attribute "to" exists
+  // - checks that it contains [object Object]
+  // On the template, the to prop is assigned to href
+  // Looking for the to prop will result in undefined
+  expect(navigationLinkStub.attributes("href")).toBeDefined();
+  expect(navigationLinkStub.attributes("href")).toContain("[object Object]");
+});
 
 it.todo("displays 'aria-current=page' when the current route matches the 'to' prop", ()=> {});
 
