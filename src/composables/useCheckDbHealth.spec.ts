f8eb94be9c637f0e80fbd6196c6a6c5a0d4a7fb0
Paz Spera <mspera03@gmail.com>
2025-11-02 18:10:25 +0000

    test: Agregado test useCheckDbHealth. Verifica respuesta success, network error y respuesta del servidor con error

--- src/composables/useCheckDbHealth.spec.ts
+++ src/composables/useCheckDbHealth.spec.ts
@@ -1,14 +1,72 @@
-import { it, describe, beforeEach, afterEach } from "vitest";
+import { it, describe, beforeEach, afterEach, vi, expect } from "vitest";
 import { useCheckDbHealth } from "./useCheckDbHealth";
 
+const runCheckHealth = async ()=> {
+  const { checkHealth, statusMessage, color, icon, hasRun } = useCheckDbHealth();
+  await checkHealth()
+  return { statusMessage, color, icon, hasRun };
+}
+
 describe("checkHealth()", () => {
+  // Arrange
   // simulate env variable
   beforeEach(()=> {
     import.meta.env.VITE_API_BASE_URL = "http://test-api.test";
   });
 
-  it.todo("check success response from fetch", {});
+  it.only("check success response from fetch", async ()=> {
+    // Act
+    // mock fetch success response
+    vi.stubGlobal("fetch", vi.fn().mockResolvedValue({
+      ok: true,
+      json: ()=> Promise.resolve({ status: "ok" }),
+    }));
+
+    // call composable
+    const { statusMessage, color, icon, hasRun } = await runCheckHealth();
+
+    // Assert
+    expect(statusMessage.value).toBe("Conectado a la base de datos");
+    expect(color.value).toBe("success");
+    expect(icon.value).toBe("faCircleCheck");
+    expect(hasRun.value).toBe(true);
+  });
   
-  it.todo("check error response from fetch", {});
+  it.only("check HTTP error response from fetch", async ()=> {
+    // Act
+    // mock fetch server returns error response
+    vi.stubGlobal("fetch", vi.fn().mockRejectedValue({
+      ok: false,
+      status: 500,
+    }))
+    
+    // call composable
+    const { statusMessage, color, icon, hasRun } = await runCheckHealth();
+
+    // Assert
+    expect(statusMessage.value).toContain("Error de conexión");
+    expect(color.value).toBe("error");
+    expect(icon.value).toBe("faCircleExclamation");
+    expect(hasRun.value).toBe(true);
+  });
+
+  it.only("check network error response from fetch", async ()=> {
+    // Act
+    // mock network error response 
+    vi.stubGlobal("fetch", vi.fn().mockRejectedValue(new Error("Falló la conexión con el servidor")));
+
+    // call composable
+    const { statusMessage, color, icon, hasRun } = await runCheckHealth();
+
+    // Assert
+    expect(statusMessage.value).toContain("Error de conexión");
+    expect(color.value).toBe("error");
+    expect(icon.value).toBe("faCircleExclamation");
+    expect(hasRun.value).toBe(true);
+  })
+
+  afterEach(()=> {
+    vi.restoreAllMocks();
+  })
 })
 
