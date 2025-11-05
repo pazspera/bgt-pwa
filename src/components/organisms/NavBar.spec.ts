65a1e9812385192342ed641af39ce5c7db41f0d5
Paz Spera <mspera03@gmail.com>
2025-11-05 03:48:02 +0000

    test: Creada función mountNavbar

--- src/components/organisms/NavBar.spec.ts
+++ src/components/organisms/NavBar.spec.ts
@@ -1,10 +1,54 @@
-import { it, describe } from "vitest";
+import { it, describe, beforeEach } from "vitest";
 import { mount } from "@vue/test-utils";
 import { router } from "../../tests/utils/createRouterMock";
+import { createVuetifyForTest } from "../../tests/utils/createVuetifyForTest";
+import { VAppBar, VNavigationDrawer, VBtn, VRow, VContainer, VList, VTooltip } from "vuetify/components";
 import NavBar from "./NavBar.vue";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+const vuetify = createVuetifyForTest({ VAppBar, VNavigationDrawer, VBtn, VRow, VContainer, VList, VTooltip });
+const faBarsText = faBars.iconName;
+
+const mountNavbar = () => {
+  return mount(NavBar, {
+    global: {
+      plugins: [vuetify],
+      stubs: {
+        "v-app-bar": {
+          template: `
+            <div data-test="v-app-bar-stub" v-bind="$attrs">
+              <slot/>
+            </div>
+          `
+        },
+        "v-navigation-drawer": {
+          template: `
+            <div data-test="v-navigation-drawer" v-bind="$attrs">
+              <slot/>
+            </div>
+          `
+        }
+      }
+    }
+  })
+}
 
 describe("NavBar Desktop", ()=> {
-  it.todo("desktop links are visible in desktop >= 1025px", ()=> {});
+  let wrapper;
+  let vAppBar;
+  let vNavigationDrawer;
+
+  beforeEach(()=> {
+    wrapper = mountNavbar();
+    vAppBar = wrapper.find('[data-test="v-app-bar-stub"]');
+    vNavigationDrawer = wrapper.find('[data-test="v-navigation-drawer"]');
+  })
+
+  it.only("desktop links are visible in desktop >= 1025px", ()=> {
+    console.log("wrapper", wrapper.html());
+    console.log("vAppBar", vAppBar.html());
+    console.log("vNavigationDrawer", vNavigationDrawer.html());
+  });
   it.todo("shows desktop layout at >= 1025px", ()=> {});
   it.todo("renders all navigation links in desktop layout", ()=> {});
 })
