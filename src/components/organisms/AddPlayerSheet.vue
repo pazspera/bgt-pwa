f7c0faa46e1fc20956393e385a26faddeecff282
Paz Spera <mspera03@gmail.com>
2025-11-10 03:48:58 +0000

    refactor: Incorporado el componente AddPlayerSheet a PlayersView

--- src/components/organisms/AddPlayerSheet.vue
+++ src/components/organisms/AddPlayerSheet.vue
@@ -1,6 +1,12 @@
 <script lang="ts" setup>
 import SubsectionTitle from '../atoms/typography/SubsectionTitle.vue';
-import { ref } from "vue";
+import { ref, defineEmits } from "vue";
+
+defineProps<{
+  modelValue: boolean
+}>()
+
+const emit = defineEmits(['update:modelValue']);
 
 const submit = () => {
   console.log(`the form was submitted with name ${name}`);
@@ -11,7 +17,10 @@ const name = ref("");
 </script>
 
 <template>
-  <v-bottom-sheet>
+  <v-bottom-sheet
+    :model-value="modelValue"
+    @update:model-value="emit('update:modelValue', $event)"
+  >
     <v-container>
       <SubsectionTitle>Agregar jugador</SubsectionTitle>
       <form @submit.prevent="submit">
--- src/views/Players/PlayersView.vue
+++ src/views/Players/PlayersView.vue
@@ -1,14 +1,26 @@
+<script setup>
+import { ref } from "vue";
+import AddPlayerSheet from "../../components/organisms/AddPlayerSheet.vue";
+
+const isSheetVisible = ref(false);
+defineOptions({ name: "PlayersView" });
+
+</script>
+
 <template>
   <v-container class="mt-4">
     <h1>Jugadores</h1>
     <router-link :to="{ name: 'AddPlayer' }">
       <v-btn color="primary">Agregar jugador</v-btn>
     </router-link>
+    <v-btn 
+      @click="isSheetVisible = !isSheetVisible"
+    >
+      Toggle AddPlayerSheet
+    </v-btn>
+
+    <AddPlayerSheet v-model="isSheetVisible" />
   </v-container>
 </template>
-
-<script setup>
-defineOptions({ name: "PlayersView" });
-</script>
 
 <style scoped></style>
