2c9f7e353a14eaaaa698e684210d7d2ed8e38507
Paz Spera <mspera03@gmail.com>
2025-11-10 03:24:47 +0000

    refactor: Creado componente AddPlayerSheet

---
+++ src/components/organisms/AddPlayerSheet.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import SubsectionTitle from '../atoms/typography/SubsectionTitle.vue';
+import { ref } from "vue";
+
+const submit = () => {
+  console.log(`the form was submitted with name ${name}`);
+}
+
+const name = ref("");
+
+</script>
+
+<template>
+  <v-bottom-sheet>
+    <v-container>
+      <SubsectionTitle>Agregar jugador</SubsectionTitle>
+      <form @submit.prevent="submit">
+        <v-text-field v-model="name" label="Nombre"></v-text-field>
+        <v-btn type="submit" color="primary">Agregar</v-btn>
+        <v-btn variant="text">Cancelar</v-btn>
+      </form>
+    </v-container>
+  </v-bottom-sheet>
+</template>
