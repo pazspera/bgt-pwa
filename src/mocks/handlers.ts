9e4fd4f17f2f1037a6e80fdead89a338d505dc17
Paz Spera <mspera03@gmail.com>
2025-11-28 20:03:05 +0000

    fix: Actualizado handlers.ts. En los mocks, tenía configurado los casos de success y error. Para los casos de error había que pasar ?status='error' por param, lo que no es útil. Eliminé todas las implementaciones de esta doble condición para los mocks. Los tests de los composables usePlayers, usePlayer y useDeletePlayer no se ven afectados ya que no estaban usando el MSW

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,5 @@
 import { http, HttpResponse } from "msw";
 import { mockPlayers } from "./data/players";
-import { mock } from "node:test";
-
 export const handlers = [
   // Check DB Health
   http.get("*/health", async({ request })=> {
@@ -9,6 +7,7 @@ export const handlers = [
     const statusParam = url.searchParams.get("status");
     
     await new Promise(resolve => setTimeout(resolve, 300));
+
 
     if(statusParam === "error") {
       return new HttpResponse(null, { status: 500 });
@@ -23,22 +22,7 @@ export const handlers = [
   http.get("*/players", async({ request }) => {
     const url = new URL(request.url);
 
-    // fix so msw doesn't interfere with loading the views
-    if(url.pathname.endsWith(".vue") || url.pathname.endsWith("js")) {
-      return;
-    }
-
-    const statusParam = url.searchParams.get("status");
-
     await new Promise(resolve => setTimeout(resolve, 600));
-
-    if(statusParam === "error") {
-      console.log("returning error from get/players");
-      return new HttpResponse(null, { 
-        status: 500,
-        statusText: "Internal Server Error"
-      });
-    };
 
     return HttpResponse.json(
       mockPlayers,
@@ -52,37 +36,23 @@ export const handlers = [
   http.get("*/players/:id", async({ params, request })=> {
     const url = new URL(request.url);
 
-    if(url.pathname.endsWith(".vue") || url.pathname.endsWith("js")) {
-      return;
-    }
-
-    const statusParam = url.searchParams.get("status");
-
     const playerId = Number(params.id);
     const player = mockPlayers.find(p => p.id === playerId);
 
     await new Promise(resolve => setTimeout(resolve, 300));
 
-    if(statusParam === "error") {
-      console.log("returning error on get/player/:id");
-      return new HttpResponse(null, {
-        status: 500,
-        statusText: "Internal Server Error"
-      })
-    }
-
-    if(statusParam === "not-found") {
-      console.log("returning player not found on get/player/:id");
+    if (!player) {
+      console.log(`get player id ${playerId} 404 not found`);
       return new HttpResponse(null, {
         status: 404,
         statusText: "Not Found"
-      })
+      });
     }
 
     return HttpResponse.json(
       player,
       {
-        status: 200, 
+        status: 200,
         statusText: "OK"
       }
     )
@@ -90,38 +60,24 @@ export const handlers = [
   // DELETE/player/:id
   http.delete("*/players/:id", async ({ params, request}) => {
     const url = new URL(request.url);
-
-    if(url.pathname.endsWith(".vue") || url.pathname.endsWith("js")) {
-      return;
-    }
-    
-    const statusParam = url.searchParams.get("status");
      
     const playerId = Number(params.id);
 
     await new Promise(resolve => setTimeout(resolve, 300));
-
-    if(statusParam === "error") {
-      console.log(`error 500 on delete for player ${playerId}`);
-      return new HttpResponse(null, {
-        status: 500,
-        statusText: "Internal Server Error"
-      })
-    };
 
     const index = mockPlayers.findIndex(p => p.id === playerId);
     
     if(index === -1) {
       console.log(`delete player id ${playerId} 404 not found`);
       return new HttpResponse(null, {
-        status: 400,
+        status: 404,
         statusText: "Not Found"
       })
     }
 
     mockPlayers.splice(index, 1);
 
-    console.log(`delete for player id ${playerId} success 200`);
+    console.log(`delete for player id ${playerId} success 204`);
     return new HttpResponse(null, {
       status: 204, 
       statusText: "No Content"
