9c1f24c8c3986f697fab59c1d7d834532eba0f69
Paz Spera <mspera03@gmail.com>
2025-12-08 04:36:44 +0000

    feature: Creados archivos iniciales para el nuevo servicio que va a llamar a la API de players

---
+++ src/api/playerApiService.ts
@@ -0,0 +1,11 @@
+import type { PlayerApiResponse, PlayersListResponse, CreatePlayerRequest, UpdatePlayerRequest } from "../types/domain/playerApi";
+
+export async function getPlayers() {}
+
+export async function getPlayer(id: string) {}
+
+export async function createPlayer() {}
+
+export async function updatePlayer() {}
+
+export async function deletePlayer(id: string) {}
