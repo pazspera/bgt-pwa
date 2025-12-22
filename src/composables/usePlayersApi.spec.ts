6d6fb1c2aa4da27422d9038ec477798182606774
Paz Spera <mspera03@gmail.com>
2025-12-22 22:29:30 +0000

    refactor: Creado nuevo archivo con data para el mock que use el tipo de PlayerApiResponse

--- src/composables/usePlayersApi.spec.ts
+++ src/composables/usePlayersApi.spec.ts
@@ -1,13 +1,25 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import * as PlayerApiService from "../api/playerApiService";
 import { mockPlayers } from "../mocks/data/players";
 import { API_ERROR_MESSAGES } from "../constants/apiErrorMessages";
 import { usePlayersApi } from "./usePlayersApi";
 
 describe("usePlayersApi", ()=> {
+  const runUsePlayers = () => {
+    const { players, totalPlayers, newPlayer, loading, error, fetchPlayers, createPlayer, deletePlayer } = usePlayersApi();
+  }
 
   describe("fetchPlayers", ()=> {
-    it("success: loads all players, updates loading status", async()=> {});
+    it.only("success: loads all players, updates loading status", async()=> {
+      // mock API service
+      vi.spyOn(PlayerApiService, "getPlayers").mockResolvedValueOnce({
+        total: 2,
+        limit: 10,
+        offset: 0,
+        data: mockPlayers,
+      })
+    });
+
     it("error not found (404): updates error and loading, returns empty array for players", async()=> {});
     it("internal server error (500): updates error and loading", async()=> {});
     it("network error: updates error and loading", async()=> {});
@@ -21,7 +33,7 @@ describe("usePlayersApi", ()=> {
   });
 
   describe("deletePlayer", ()=> {
-    it.each([200,204])("success: delete returns %s", async()=> {});
+    it.each([200,204])("success: delete updates loading status", async()=> {});
     it("error not found(404): updates error and loading", async()=> {});
     it("internal server error (500): updates error and loading", async()=> {});
     it("network error: updates error and loading", async()=> {});
---
+++ src/mocks/data/playersApi.ts
@@ -0,0 +1,31 @@
+import type { PlayerApiResponse } from "../../types/domain/playerApi"
+export const mockPlayers: PlayerApiResponse[] = [
+  { 
+    id: "1", 
+    name: "Zeuchi", 
+    created_at: "1999-10-10-0:00:00Z",
+    updated_at: "1999-10-10-0:00:00Z",
+    is_registered: false,
+  },
+  { 
+    id: "2", 
+    name: "Mareita", 
+    created_at: "1999-10-10-0:00:00Z",
+    updated_at: "1999-10-10-0:00:00Z",
+    is_registered: false,
+  },
+  { 
+    id: "3", 
+    name: "Pini", 
+    created_at: "1999-10-10-0:00:00Z",
+    updated_at: "1999-10-10-0:00:00Z",
+    is_registered: false,
+  },
+  { 
+    id: "4", 
+    name: "Paz", 
+    created_at: "1999-10-10-0:00:00Z",
+    updated_at: "1999-10-10-0:00:00Z",
+    is_registered: false,
+  },
+]
