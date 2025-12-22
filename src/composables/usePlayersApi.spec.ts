2c09bf566def25325c064d12802340737ec5e2f7
Paz Spera <mspera03@gmail.com>
2025-12-22 23:34:24 +0000

    tests: Agregado test para caso success que no devuelve players en fetchPlayers

--- src/composables/usePlayersApi.spec.ts
+++ src/composables/usePlayersApi.spec.ts
@@ -12,7 +12,7 @@ describe("usePlayersApi", ()=> {
   })
 
   describe("fetchPlayers", ()=> {
-    it.only("success: loads all players, updates loading status", async()=> {
+    it("success: loads all players, updates loading status", async()=> {
       // mock API service
       const getPlayersSpy = vi.spyOn(PlayerApiService, "getPlayers").mockResolvedValueOnce(mockPlayersListResponse);
 
@@ -38,7 +38,37 @@ describe("usePlayersApi", ()=> {
       expect(players.value).toEqual(mockPlayersApi);
     });
 
-    it("error not found (404): updates error and loading, returns empty array for players", async()=> {});
+    it("success no players: updates loading status and players correctly with empty list", async()=> {
+      // mock API service
+      const getPlayersSpy = vi.spyOn(PlayerApiService, "getPlayers").mockResolvedValueOnce({
+        total: 0,
+        limit: 0,
+        offset: 0,
+        data: []
+      });
+
+      const { players, totalPlayers, loading, error, fetchPlayers } = usePlayersApi();
+
+      // test initial state
+      expect(loading.value).toBe(false);
+      expect(players.value).toEqual([]);
+
+      const promise = fetchPlayers();
+
+      // starts loading and resets errors
+      expect(loading.value).toBe(true);
+      expect(error.value).toBeNull();
+
+      await promise;
+
+      // test end state
+      expect(getPlayersSpy).toHaveBeenCalledTimes(1);
+      expect(loading.value).toBe(false);
+      expect(error.value).toBeNull();
+      expect(totalPlayers.value).toBe(0);
+      expect(players.value).toEqual([]);
+    });
+
     it("internal server error (500): updates error and loading", async()=> {});
     it("network error: updates error and loading", async()=> {});
   });
