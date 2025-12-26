fd649756864f2da42a31d64c88b761b2eca71794
Paz Spera <mspera03@gmail.com>
2025-12-26 14:44:10 +0000

    feature: Creado componente CardGrid. Armado solamente para mostrar las cards de Players. Después se van a agregar las cards para mostrar boardgames y games, de esa manera el mismo componente puede mostrar grillas de todos los distintos tipos de cards que se van a utilizar

---
+++ src/components/organisms/CardGrid.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import type { PlayerApiResponse } from '../../types/domain/playerApi';
+import { onBeforeMount } from 'vue';
+import PlayerCard from '../molecules/PlayerCard.vue';
+
+type GridItem = PlayerApiResponse;
+
+// definir un or con los distintos tipos de card que puede recibir
+const props = defineProps<{
+  data: GridItem[],
+  type: "player" | "boardgame" | "game",
+}>();
+
+onBeforeMount(()=> {
+  console.log(props.data);
+})
+
+</script>
+
+<template>
+  <h2>Card grid</h2>
+
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="item in data"
+        :key="item.id"
+      >
+        <PlayerCard v-if="type === 'player'" :player="item" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
