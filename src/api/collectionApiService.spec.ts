9e4ff03edc87d8d3f74c51a86d45415455721942
Paz Spera <mspera03@gmail.com>
2026-01-05 20:49:01 +0000

    tests: Definidos tests para getCollections() de collectionApiService

---
+++ src/api/collectionApiService.spec.ts
@@ -0,0 +1,14 @@
+import { it, vi, describe, expect, afterEach } from "vitest";
+
+
+describe("collectionApiService: getCollections()", ()=> {
+  it.only("success: returns list of all boardgames", async()=> {
+    // crear archivo mockeando respuesta
+    // se tiene que llamar al método 1 con get
+    // tiene que devolver la lista de boardgames
+  });
+
+  it.todo("error: resource not found (404)", async()=> {});
+  it.todo("error: internal server error (500)", async()=> {});
+  it.todo("error: network error", async()=> {});
+})
