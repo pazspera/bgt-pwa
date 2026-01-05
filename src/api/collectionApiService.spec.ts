13f451339967c15b411a2a3d78eb3bbe8d9d2f0a
Paz Spera <mspera03@gmail.com>
2026-01-05 21:18:42 +0000

    tests: Agregados tests para collectionApiService, getCollections()

--- src/api/collectionApiService.spec.ts
+++ src/api/collectionApiService.spec.ts
@@ -1,14 +1,38 @@
 import { it, vi, describe, expect, afterEach } from "vitest";
+import { mockCollectionsListResponse } from "../mocks/data/collectionsApi";
+import { getCollections } from "./collectionApiService";
+import { API_ERROR_MESSAGES } from "../constants/apiErrorMessages";
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 describe("collectionApiService: getCollections()", ()=> {
-  it.only("success: returns list of all boardgames", async()=> {
-    // crear archivo mockeando respuesta
-    // se tiene que llamar al método 1 con get
-    // tiene que devolver la lista de boardgames
+  afterEach(()=> {
+    vi.restoreAllMocks();
+    vi.clearAllMocks();
+  })
+
+  it("success: returns list of all boardgames", async()=> {
+    vi.spyOn(global, "fetch").mockResolvedValue({
+      ok: true,
+      json: async ()=> mockCollectionsListResponse,
+    } as unknown as Response);
+
+    const result = await getCollections();
+
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith(`${API_BASE_URL}/collections/primary/games`, { method: "GET" });
+    expect(result).toEqual(mockCollectionsListResponse);
   });
 
-  it.todo("error: resource not found (404)", async()=> {});
-  it.todo("error: internal server error (500)", async()=> {});
-  it.todo("error: network error", async()=> {});
+  it("error: internal server error (500)", async()=> {
+    vi.spyOn(global, "fetch").mockRejectedValue(new TypeError(API_ERROR_MESSAGES.GET_COLLECTIONS_FAILED(500)));
+
+    await expect(getCollections()).rejects.toThrow(API_ERROR_MESSAGES.GET_COLLECTIONS_FAILED(500));
+  });
+
+  it("error: network error", async()=> {
+    vi.spyOn(global, "fetch").mockRejectedValue(new TypeError(API_ERROR_MESSAGES.NETWORK_ERROR));
+
+    await expect(getCollections()).rejects.toThrow(API_ERROR_MESSAGES.NETWORK_ERROR);
+  });
 })
