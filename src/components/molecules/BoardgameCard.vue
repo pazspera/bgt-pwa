0202ecca74a98dd95f44db839f68944d8200f2d4
Paz Spera <mspera03@gmail.com>
2026-01-06 14:46:20 +0000

    refactor: Agregado btn de agregar partida y estilstilos a BoardgameCard

--- src/components/molecules/BoardgameCard.vue
+++ src/components/molecules/BoardgameCard.vue
@@ -12,15 +12,32 @@ const props = defineProps<{
 <template>
   <v-card
     data-testid="boardgame-card"
+    class="boardgame-card"
     variant="elevated"
     hover
   >
-    <v-card-item>
+    <v-card-title>
       {{ boardgame.name }}
-    </v-card-item>
+    </v-card-title>
 
-    
+    <v-card-actions>
+      <v-btn
+        color="primary"
+        variant="plain"
+      >
+        <FontAwesomeIcon :icon="faPlus" />
+        <span>Agregar partida</span>
+      </v-btn>
+    </v-card-actions>
+
   </v-card>
 </template>
 
-<style></style>
+<style scoped>
+.boardgame-card {
+  display: flex;
+  flex-direction: column;
+  align-items: self-start;
+  padding: 12px;
+}
+</style>
