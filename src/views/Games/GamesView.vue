b6b62956971fbd6e62199ce72fc5ac576a9e8164
Paz Spera <mspera03@gmail.com>
2026-01-10 21:02:17 +0000

    refactor: Prueba guardar nueva partida en GamesView, funciona correctamente

--- src/views/Games/GamesView.vue
+++ src/views/Games/GamesView.vue
@@ -1,11 +1,45 @@
+<script setup lang="ts">
+import * as GameApiService from "../../api/gameApiService";
+import { onBeforeMount } from "vue";
+import { CreateGameRequest } from "../../types/domain/gamesApi";
+
+defineOptions({ name: "GamesView" });
+
+onBeforeMount(()=> {
+const today = new Date().toISOString();
+console.log(today);
+
+const newGame: CreateGameRequest = {
+  boardgame_id: "bcae2afc-f027-489d-8f70-fd8f79d10533",
+  collection_id: "b6acc73a-6b7a-4c67-937a-e1a6169f173f",
+  player_group_id: null,
+  start_date: today,
+  end_date: null,
+  notes: "Guardando una nueva partida",
+  players: [
+    {
+      player_id: "3a28cf09-0506-4d1d-aba7-414702eb690c",
+      is_winner: true,
+    },
+    {
+      player_id: "2b0164f7-1a93-491e-aced-ba04c4034157",
+      is_winner: false,
+    }
+  ]
+}
+
+console.log(newGame);
+
+GameApiService.createGame(newGame);
+
+})
+
+</script>
+
 <template>
   <v-container class="mt-4">
     <h1>Partidas</h1>
   </v-container>
 </template>
-
-<script setup>
-defineOptions({ name: "GamesView" });
-</script>
 
 <style scoped></style>
