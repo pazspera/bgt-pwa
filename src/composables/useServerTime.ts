2e84332021c032ca18c720a04af946b7f6a20b88
Paz Spera <mspera03@gmail.com>
2026-01-20 22:11:52 +0000

    refactor: Agregada validación que mantenga el timeOffset igual si se recibe un string inválido en el tiempo del servidor

--- src/composables/useServerTime.ts
+++ src/composables/useServerTime.ts
@@ -7,6 +7,11 @@ export function useServerTime() {
   const syncWithServer = (serverTime: string) => {
     const serverTimeInMilliseconds = new Date(serverTime).getTime();
     const clientTimeInMilliseconds = new Date().getTime();
+    
+    if(isNaN(serverTimeInMilliseconds)) {
+      timeOffset.value = 0;
+      return;
+    }
 
     timeOffset.value = serverTimeInMilliseconds - clientTimeInMilliseconds;
   };
