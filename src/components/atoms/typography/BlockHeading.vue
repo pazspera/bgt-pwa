b768e6608939b93420820b8ccfe429daac9522e8
Paz Spera <mspera03@gmail.com>
2026-02-03 18:11:18 +0000

    style: Revisados estilos para SubsectionTitle y BlockHeading

--- src/components/atoms/typography/BlockHeading.vue
+++ src/components/atoms/typography/BlockHeading.vue
@@ -12,38 +12,38 @@ defineOptions({ name: "BlockHeading" });
 .block-heading {
   font-family: var(--font-title);
   font-size: var(--font-size-4xl);
-  font-weight: 700;
+  font-weight: 500;
   line-height: 1.25;
   margin-bottom: 0.5em;
 }
 
 @media (max-width: 600px) {
   .block-heading {
-    font-size: var(--font-size-xl);
+    font-size: var(--font-size-lg);
   }
 }
 
 @media (min-width: 601px) and (max-width: 960px) {
   .block-heading  {
-    font-size: var(--font-size-2xl);
+    font-size: var(--font-size-xl);
   }
 }
 
 @media (min-width: 961px) and (max-width: 1280px) { 
   .block-heading  {
-    font-size: var(--font-size-3xl);
+    font-size: var(--font-size-2xl);
   }
 }
 
 @media (min-width: 1281px) and (max-width: 1920px) {
   .block-heading  {
-    font-size: var(--font-size-4xl);
+    font-size: var(--font-size-3xl);
   }
 }
 
 @media (min-width: 1921px) {
   .block-heading  {
-    font-size: var(--font-size-5xl);
+    font-size: var(--font-size-4xl);
   }
 }
 </style>
