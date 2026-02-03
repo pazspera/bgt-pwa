caf07abef7a1d9c87f724d7ab8a77fd4cf0fe68a
Paz Spera <mspera03@gmail.com>
2026-02-03 21:59:49 +0000

    refactor: Agregada columna para anotar cuáles componentes están usando cada tipografía

--- src/components/molecules/TypographyChart.vue
+++ src/components/molecules/TypographyChart.vue
@@ -23,7 +23,8 @@ import ExternalLink from '../atoms/typography/ExternalLink.vue';
         <tr>
           <th>HTML Tag</th>
           <th>Componente</th>
-          <th>Uso</th>
+          <th>Descripción</th>
+          <th>Componentes</th>
         </tr>
       </thead>
       <tbody>
@@ -31,71 +32,85 @@ import ExternalLink from '../atoms/typography/ExternalLink.vue';
           <td>H1</td>
           <td><display-title>Display Title</display-title></td>
           <td>Page title</td>
+          <td></td>
         </tr>
         <tr>
           <td>H2</td>
           <td><section-title>Section Title</section-title></td>
           <td>Section title</td>
+          <td></td>
         </tr>
         <tr>
           <td>H3</td>
           <td><subsection-title>Subsection Title</subsection-title></td>
           <td>Subsection title</td>
+          <td></td>
         </tr>
         <tr>
           <td>H4</td>
           <td><block-heading>Block Heading</block-heading></td>
           <td>Block heading</td>
+          <td></td>
         </tr>
         <tr>
           <td>H5</td>
           <td><card-heading>Card Heading</card-heading></td>
           <td>Card heading</td>
+          <td></td>
         </tr>
         <tr>
           <td>H6</td>
           <td><minor-heading>Minor Heading</minor-heading></td>
           <td>Minor heading</td>
+          <td></td>
         </tr>
         <tr>
           <td>P</td>
           <td><lead-text>Lead Text</lead-text></td>
           <td>Subtitle 1</td>
+          <td></td>
         </tr>
         <tr>
           <td>P</td>
           <td><support-text>Support Text</support-text></td>
           <td>Subtitle 2</td>
+          <td></td>
         </tr>
         <tr>
           <td>P</td>
           <td><body-text>Body Text</body-text></td>
           <td>Body text</td>
+          <td></td>
         </tr>
         <tr>
           <td>P</td>
           <td><detail-text>Detail Text</detail-text></td>
           <td>Detail text</td>
+          <td></td>
         </tr>
         <tr>
           <td>P</td>
           <td><caption-text>Caption Text</caption-text></td>
           <td>Caption text</td>
+          <td></td>
         </tr>
         <tr>
           <td>Span</td>
           <td><button-label>Button Label</button-label></td>
           <td>Button label</td>
+          <td></td>
         </tr>
         <tr>
           <td>Router Link</td>
           <td><navigation-link :to="{ name: 'BoardGames' }">Navigation Link</navigation-link></td>
           <td>Navigation Link</td>
+          <td></td>
         </tr>
         <tr>
           <td>A</td>
           <td><external-link :href="'#'" :target="'_blank'">External Link</external-link></td>
           <td>External link</td>
+          <td></td>
         </tr>
       </tbody>
     </v-table>
