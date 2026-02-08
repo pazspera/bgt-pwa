bfcd9d6311fc747a2ed0ab16eeeea71c2a378c1d
Paz Spera <mspera03@gmail.com>
2026-02-08 00:11:40 +0000

    tests: Definidos los tests para AppSnackbar. Estaba generando problemas el wrapper porque el componente de snackbar usa teleport para visualizarse en el DOM. Definir el stub para v-snackbar permite ver el html y contenido del wrapper

--- src/components/molecules/AppSnackbar.spec.ts
+++ src/components/molecules/AppSnackbar.spec.ts
@@ -1,11 +1,5 @@
-import { ref, nextTick } from "vue";
-import { it, expect, vi, describe } from "vitest";
-const flushPromises = require("flush-promises");
+import { it, expect, vi, describe, beforeEach } from "vitest";
 
-/* 
-  These tests are skipped. After many, many tries, the
-  component doesn't rendered correctly. 
-*/
 
 // The snackbar is not stubbed and needs this object to work
 globalThis.visualViewport = {
@@ -23,47 +17,27 @@ globalThis.visualViewport = {
   dispatchEvent: (_event: Event) => true
 };
 
-/* 
-  IMPORTANT
-  - the watch in AppSnackbar doesn't activate, so it renders
-  the v-if instead of the component
-  - sharedHasRun has to be here to make sure the test doesn't break
-  - what worked was wrapper.vm.snackbarVisible = true;
-  - this changes the value of the watch directly
-*/
-const sharedHasRun = ref(false);
-
-// initial mock
-vi.mock("@/composables/useCheckDbHealth", ()=> ({
-  useCheckDbHealth: ()=> ({
-    statusMessage: ref("Conectado a la base de datos"),
-    color: ref("success"),
-    icon: ref("faCircleCheck"),
-    hasRun: sharedHasRun,
-    checkHealth: vi.fn(),
-  })
-}));
-
 import { mount } from "@vue/test-utils";
 import AppSnackbar from "./AppSnackbar.vue";
 import { createVuetifyForTest } from "../../tests/utils/createVuetifyForTest";
 import { VSnackbar } from "vuetify/components";
-// the composable still needs to be imported
-// when it's used later on, the mock will be used instead
-import { useCheckDbHealth } from "../../composables/useCheckDbHealth";
 
-const vuetify = createVuetifyForTest();
+const vuetify = createVuetifyForTest({ VSnackbar });
 
 // Arrange
 // function to mount component
-const mountAppSnackbar = ()=> {
+const mountAppSnackbar = (props = {})=> {
   return mount(AppSnackbar, {
     attachTo: document.body,
+    props: {
+      visible: true,
+      message: "Test message",
+      color: "info",
+      timeout: 5000,
+      ...props
+    },
     global: {
       plugins: [vuetify],
-      components: {
-        VSnackbar
-      },
       stubs: {        
         "v-btn": {
           template: `
@@ -72,6 +46,12 @@ const mountAppSnackbar = ()=> {
             </button>
           `
         },
+        "v-snackbar": { 
+            props: ["modelValue", "color"], 
+            template: `
+              <div v-if="modelValue" class="mock-snackbar" :data-color="color"> 
+                <slot /> <slot name="actions" /> 
+              </div>` },
         FontAwesomeIcon: {
           props: ["icon"],
           template: `<i :data-icon="icon"></i>`
@@ -81,57 +61,30 @@ const mountAppSnackbar = ()=> {
   });
 }
 
-describe("", ()=> {
-  it("", ()=> {})
-  it("", ()=> {})
-  it("", ()=> {})
-  it("", ()=> {})
+
+
+describe("rendering", ()=> {
+  let wrapper;
+
+  beforeEach(()=> {
+    wrapper = mountAppSnackbar();
+  })
+
+
+  it("renders correct message", ()=> {
+    console.log(wrapper.html())
+  })
+  it("renders correct color", ()=> {})
+  it("renders correct timeout value", ()=> {})
+  it("renders the 'close' button", ()=> {})
 })
 
-describe("mock response from useCheckDbHealth()", ()=> {
-
-  it.skip("shows correct info on success response", async ()=> {
-    // Act
-    // Fixes teleporting issue
-    const teleportTarget = document.createElement("div");
-    teleportTarget.setAttribute("data-app", "true");
-    document.body.appendChild(teleportTarget);
-
-    sharedHasRun.value = false;
-    const wrapper = mountAppSnackbar();
-    // changes the value of the watch directly 
-    wrapper.vm.snackbarVisible = true;
-    sharedHasRun.value = true;
-
-    await flushPromises();
-    await nextTick();
-
-    console.log("snackbarVisible:", wrapper.vm.snackbarVisible);
-    // Since it teleports, wrapper.html() doesn't show the component
-    // Need to use document.body.innerHTML and document.body.textContent
-    console.log("document.body.innerHTML:");
-    console.log(document.body.innerHTML);
-
-    console.log("document.body.textContent:");
-    console.log(document.body.textContent);
-    
-    // Assert
-    
-    // The component doesn't render correctly
-    // Can't test statusMessage or that the color change
-    // Got this far, I'm done. This component will be deleted anyway
-
-
-  });
-
-  it.skip("shows correct info on error response", ()=> {
-    // Assert
-    // función mock error response
-    // llamada a función mount
-    
-    // Act
-
-    // Assert
-  });
+describe("component logic", ()=> {
+  it("emits 'close' event when close button is clicked", ()=> {})
+  it("updates model-value when close button is clicked", ()=> {});
+  it("emits 'close' event on timeout", ()=> {})
+  it("AppSnackbar is not visible if no message is received", ()=> {
+    // check message undefined, null or ""
+  })
 })
-it.skip("closes when close button is clicked", ()=> {});
+
