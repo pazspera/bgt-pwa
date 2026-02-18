dcd1518e4437e94622f310f6e78951562b5a42dd
pinic0 <pinicodor@gmail.com>
2026-02-17 23:30:54 -0300

    llamada al servicio de partidas

--- src/api/gameApiService.ts
+++ src/api/gameApiService.ts
@@ -28,3 +28,19 @@ export async function createGame(newGame: CreateGameRequest) {
 
 
 }
+
+export async function getGames(limit: number = 10, offset: number = 0): Promise<GamesListResponse> {
+  try {
+    const response = await fetch(`${API_BASE_URL}v1/games?limit=${limit}&offset=${offset}&include=players`, { method: "GET" });
+
+    if(!response.ok) {
+      //throw new Error(API_ERROR_MESSAGES.GET_GAMES_FAILED(response.status));
+      throw new Error(`${response.status}: No se pudieron obtener las partidas`);
+    }
+
+    const data: GamesListResponse = await response.json();
+    return data;
+  } catch (err) {
+    throw new Error(API_ERROR_MESSAGES.UNKNOWN_ERROR(err?.message));
+  }
+}
