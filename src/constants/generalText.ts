e72c07671f6281d0ef94df05a282312d635930ef
Paz Spera <mspera03@gmail.com>
2026-04-29 01:25:24 -0300

    refactor: Agregados mensajes para los casos donde no hay datos disponibles de las vistas

--- src/constants/generalText.ts
+++ src/constants/generalText.ts
@@ -1,3 +1,6 @@
 export const GENERAL_UI_TEXT = {
-  ALERT_ERROR: "¡Oh, no! Ocurrió un error"
+  ALERT_ERROR: "¡Oh, no! Ocurrió un error",
+  NO_DATA_PLAYERS: "No hay jugadorxs todavía",
+  NO_DATA_GAMES: "Todavía no hay partidas guardadas",
+  NO_DATA_BOARDGAMES: "No hay juegos en la ludoteca",
 }
