d00f8ea5ca4c45b037087201c654bc527951bc5e
Paz Spera <mspera03@gmail.com>
2026-04-29 22:42:29 -0300

    feature: Agregado caso donde no hay boardgames disponibles para mostrar en la vista de Boardgames. Agregado mensaje de CTA para incluir junto al título

--- src/constants/generalText.ts
+++ src/constants/generalText.ts
@@ -1,6 +1,7 @@
 export const GENERAL_UI_TEXT = {
   ALERT_ERROR: "¡Oh, no! Ocurrió un error",
-  NO_DATA_PLAYERS: "No hay jugadorxs todavía",
-  NO_DATA_GAMES: "Todavía no hay partidas guardadas",
-  NO_DATA_BOARDGAMES: "No hay juegos en la ludoteca",
+  NO_DATA_PLAYERS_TITLE: "No hay jugadorxs todavía",
+  NO_DATA_GAMES_TITLE: "Todavía no hay partidas guardadas",
+  NO_DATA_BOARDGAMES_TITLE: "No hay juegos en la ludoteca",
+  NO_DATA_BOARDGAMES_CTA: "Todavía no se pueden agregar juegos, estamos trabajando para lograrlo",
 }
--- src/views/BoardGames/BoardGamesView.vue
+++ src/views/BoardGames/BoardGamesView.vue
@@ -55,12 +55,18 @@ onBeforeMount(async () => {
     <!-- loading -->
     <LoadingRow v-else-if="loadingList && (!collection || totalBoardgames === 0) && !errorFetchCollections" />
 
-    <CardGrid 
-      v-else-if="(collection && collection.length > 0) && !errorFetchCollections" 
+    <CardGrid
+      v-else-if="(collection && collection.length > 0) && !errorFetchCollections"
       :data="collection"
-      type="boardgame" 
+      type="boardgame"
       @add-game="handleAddGame"
     ></CardGrid>
+
+    <!-- no boardgames to show -->
+    <v-row v-else-if="!errorFetchCollections">
+      <SubsectionTitle>{{ GENERAL_UI_TEXT.NO_DATA_BOARDGAMES_TITLE }}</SubsectionTitle>
+      <BodyText>{{ GENERAL_UI_TEXT.NO_DATA_BOARDGAMES_CTA }}</BodyText>
+    </v-row>
 
     <!-- snackbar for saved game -->
     <AppSnackbar
@@ -71,12 +77,12 @@ onBeforeMount(async () => {
       @close="hide()"
     />
 
-    <AddGameDialog 
-      v-if="selectedBoardgame" 
-      v-model="isAddGameDialogOpen" 
+    <AddGameDialog
+      v-if="selectedBoardgame"
+      v-model="isAddGameDialogOpen"
       :boardgame="selectedBoardgame"
       @success="handleSaveSuccess"
-      @error="handleSaveError" 
+      @error="handleSaveError"
     />
   </v-container>
 </template>
