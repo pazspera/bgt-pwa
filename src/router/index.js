6c1d73c83b0e1bd16cb7819ac3f3aa8c0a5926b6
Paz Spera <mspera03@gmail.com>
2026-04-29 22:33:44 -0300

    chore: Actualizado router para que no incluya el path a las vistas borradas

--- src/router/index.js
+++ src/router/index.js
@@ -13,21 +13,9 @@ export const routes = [
     component: () => import(/* webpackChunkName: "Players"*/ "@/views/Players/PlayersView.vue"),
   },
   {
-    path: "/editar-jugador/:id?",
-    name: "EditPlayer",
-    component: () => import(/*webpackChunkName: "EditPlayer"*/ "@/views/Players/EditPlayer.vue"),
-    props: true,
-  },
-  {
     path: "/partidas",
     name: "Games",
     component: () => import(/*webpackChunkName: "Games" */ "@/views/Games/GamesView.vue"),
-  },
-  {
-    path: "/agregar-partida",
-    name: "AddGame",
-    component: () => import(/*webpackChunkName: "AddGame" */ "@/views/Games/AddGame.vue"),
-    props: true,
   },
   {
     path: "/tipografia",
