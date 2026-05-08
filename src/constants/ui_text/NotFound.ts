3f7d98b951f557c904e9a31f33b0af6b34636647
Paz Spera <mspera03@gmail.com>
2026-05-08 19:40:08 -0300

    refactor: Actualizado texto de botón de vista NotFound

--- src/constants/ui_text/NotFound.ts
+++ src/constants/ui_text/NotFound.ts
@@ -2,5 +2,5 @@ export const NOT_FOUND = {
   TITLE: "¡Oh,no!",
   TEXT_1: "La página que estás buscando no está disponible o no existe.",
   TEXT_2: "Como diría Alejandro Lerner, es momento de volver a empezar.",
-  BTN_TEXT: "Volver al inicio"
+  BTN_TEXT: "Volver a la ludoteca"
 }
