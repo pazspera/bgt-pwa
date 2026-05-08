4f0b0f8e8fc17ed9faa693efea7f0e752dcc3e6e
Paz Spera <mspera03@gmail.com>
2026-05-08 17:50:41 -0300

    feature: Agregadas constantes con texto para 404. Agregado maquetado del contenido a la vista 404

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import DisplayTitle from '@/components/atoms/typography/DisplayTitle.vue';
+import LeadText from '@/components/atoms/typography/LeadText.vue';
+import AppButton from '@/components/atoms/buttons/AppButton.vue';
+import { NOT_FOUND } from '@/constants/ui_text/NotFound';
+</script>
 
 <template>
-  <h1>Not found</h1>
+  <v-container class="my-4 my-md-6 my-lg-8">
+    <v-row>
+      <v-col>
+        <DisplayTitle>{{ NOT_FOUND.TITLE }}</DisplayTitle>
+        <div class="mb-6">
+          <LeadText>{{ NOT_FOUND.TEXT_1 }}</LeadText>
+          <LeadText>{{ NOT_FOUND.TEXT_2 }}</LeadText>
+        </div>
+        <AppButton
+          size="large"
+          :label="NOT_FOUND.BTN_TEXT"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
