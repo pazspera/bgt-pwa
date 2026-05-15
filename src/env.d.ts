a579047404b60659f6376725a6babb2cc4296d6e
Nico <pinicodor@gmail.com>
2026-05-15 12:13:56 -0300

    useCheckDbHealth invoca /api/v1/health

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,6 @@
 // <reference types="vite/client" />
 interface ImportMetaEnv {
-  VITE_API_BASE_URL: string,
+VITE_API_BASE_URL: string,
   VITE_API_HEALTH_BASE_URL: string,
   VITE_MOCK_API: string,
 }
