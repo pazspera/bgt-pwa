06acf5fd8bb65be1df0964bf755679ca66ad2b32
Paz Spera <mspera03@gmail.com>
2026-05-28 01:10:56 -0300

    refactor: Validación para el edge case donde, al borrar una partida, la página en la que está el usuario desaparece.

--- src/views/Games/GamesView.vue
+++ src/views/Games/GamesView.vue
@@ -60,6 +60,18 @@ const confirmDelete = async () => {
     await deleteGame(selectedGame.value.id);
 
     // updates ui
+    // verifies edge case where by deleting a game,
+    // the page disappears
+    if(currentPage.value > totalPages.value && totalPages.value > 0) {
+      const lastPage = totalPages.value;
+      if(lastPage === 1) {
+        // doesn't show page=1 in url
+        router.push({ query: {} });
+      } else {
+        router.push({ query: { page: String(lastPage) } });
+      }
+    }
+
     await getGames(currentPage.value);
 
     success(GAME_STATUS.DELETED)
